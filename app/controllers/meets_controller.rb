d480502a62dd2c821f2a9e14d3a177878d6aef2a
Jennifer Coryell <jennifer.coryell@gmail.com>
2013-02-21 14:42:36 -0800

    Add Create meet view

--- app/controllers/meets_controller.rb
+++ app/controllers/meets_controller.rb
@@ -13,12 +13,18 @@ class MeetsController < ApplicationController
   end
   
   def new
-    @event_types = RaceTypes.all
-
+    @event_types = RaceType.all
+    @meet = Meet.new
   end
 
   def create
-
+    @meet = Meet.new(params[:meet])
+    if @meet.save
+      redirect_to team_new_path(:meet => @meet.id)
+    else
+      render 'new'
+    end
+    
   end
 
   def destroy
